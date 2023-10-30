1d4647f50bdae14bd85105d411c18e69ffd98eda
leeyanqing2004 <yanqing.lee@mail.utoronto.ca>
2023-10-30 12:59:53 -0400

    Added first getByCuisine API call

--- src/data_access/SearchRecipeDataAccessObject.java
+++ src/data_access/SearchRecipeDataAccessObject.java
@@ -1,4 +1,21 @@
 package data_access;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import use_case.signup.SignupInputData;
+
 public class SearchRecipeDataAccessObject {
+
+    public void getByCuisine(String cuisine) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.spoonacular.com/recipes/complexSearch?apiKey=c8eb3493121949b592c890859e2a5240&query=pasta&maxFat=25&number=2")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+    }
 }
