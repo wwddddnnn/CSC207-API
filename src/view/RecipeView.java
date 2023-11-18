f14ac90da6243475e4873c9a3df312529f8af2ea
Tiger Jin <tiger.jin@mail.utoronto.ca>
2023-11-17 20:17:46 -0500

    updated RecipeView

--- src/view/RecipeView.java
+++ src/view/RecipeView.java
@@ -12,50 +12,55 @@ public class RecipeView {
     }
 
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Recipe Management App");
+        JFrame frame = new JFrame("Recipe Search Screen");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Creating the layout and components
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.insets = new Insets(10, 20, 10, 60);
+
+        JLabel instructionLabel = new JLabel("Please enter the search filters you would like to apply for your recipe");
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        panel.add(instructionLabel, constraints);
 
         // Query field
-        JLabel queryLabel = new JLabel("Query:");
+        JLabel queryLabel = new JLabel("Query");
         constraints.gridx = 0;
-        constraints.gridy = 0;
+        constraints.gridy = 1;
         panel.add(queryLabel, constraints);
 
         JTextField queryTextField = new JTextField(20);
-        constraints.gridx = 1;
+        constraints.gridx = 2;
         panel.add(queryTextField, constraints);
 
         // Cuisine field
-        JLabel cuisineLabel = new JLabel("Cuisine:");
-        constraints.gridy = 1;
+        JLabel cuisineLabel = new JLabel("Cuisine (Optional)");
+        constraints.gridy = 2;
         constraints.gridx = 0;
         panel.add(cuisineLabel, constraints);
 
         JTextField cuisineTextField = new JTextField(20);
-        constraints.gridx = 1;
+        constraints.gridx = 2;
         panel.add(cuisineTextField, constraints);
 
         // Weight field
-        JLabel weightLabel = new JLabel("Weight:");
-        constraints.gridy = 2;
+        JLabel weightLabel = new JLabel("Max time required to make the recipe (Optional)");
+        constraints.gridy = 3;
         constraints.gridx = 0;
         panel.add(weightLabel, constraints);
 
         JTextField weightTextField = new JTextField(20);
-        constraints.gridx = 1;
+        constraints.gridx = 2;
         panel.add(weightTextField, constraints);
 
         // Search button
         JButton searchButton = new JButton("Search");
-        constraints.gridy = 3;
-        constraints.gridx = 0;
-        constraints.gridwidth = 2;
+        constraints.gridy = 4;
+        constraints.gridx = 1;
+        constraints.gridwidth = 1;
         panel.add(searchButton, constraints);
 
         // Action listener for the search button
